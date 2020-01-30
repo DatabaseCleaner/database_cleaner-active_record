9b5768ffc2595b01c85ef9c67628dd8ff5c3f1a6
Micah Geisel <micah@botandrose.com>
2020-01-30 09:36:33 -0800

    remove support for dead mysql gem.

--- spec/support/active_record_helper.rb
+++ spec/support/active_record_helper.rb
@@ -2,9 +2,14 @@ require 'active_record'
 require 'database_cleaner/spec/database_helper'
 
 class ActiveRecordHelper < DatabaseCleaner::Spec::DatabaseHelper
-  def setup
-    patch_mysql_adapters
+  # remove when database_cleaner is 2.0
+  def self.with_all_dbs &block
+    %w[mysql2 sqlite3 postgres].map(&:to_sym).each do |db|
+      yield new(db)
+    end
+  end
 
+  def setup
     Kernel.const_set "User", Class.new(ActiveRecord::Base)
     Kernel.const_set "Agent", Class.new(ActiveRecord::Base)
 
@@ -28,13 +33,6 @@ class ActiveRecordHelper < DatabaseCleaner::Spec::DatabaseHelper
   def establish_connection(config = default_config)
     ActiveRecord::Base.establish_connection(config)
     @connection = ActiveRecord::Base.connection
-  end
-
-  def patch_mysql_adapters
-    # remove DEFAULT NULL from column definition, which is an error on primary keys in MySQL 5.7.3+
-    primary_key_sql = "int(11) auto_increment PRIMARY KEY"
-    ActiveRecord::ConnectionAdapters::MysqlAdapter::NATIVE_DATABASE_TYPES[:primary_key] = primary_key_sql
-    ActiveRecord::ConnectionAdapters::Mysql2Adapter::NATIVE_DATABASE_TYPES[:primary_key] = primary_key_sql
   end
 end
 

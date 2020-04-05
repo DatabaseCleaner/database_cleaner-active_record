c1eebf7e59ba465055ec53d7b96e9065ec986eb3
Micah Geisel <micah@botandrose.com>
2020-04-05 12:23:28 -0700

    requiring this gem must explicitly configure DatabaseCleaner to use it, now that autodetection is out.

--- lib/database_cleaner/active_record.rb
+++ lib/database_cleaner/active_record.rb
@@ -3,3 +3,5 @@ require 'database_cleaner/core'
 require 'database_cleaner/active_record/deletion'
 require 'database_cleaner/active_record/transaction'
 require 'database_cleaner/active_record/truncation'
+
+DatabaseCleaner[:active_record].strategy = :transaction

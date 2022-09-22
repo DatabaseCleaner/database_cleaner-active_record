998c1f3aed5535ff4377e68f961343006873518a
hiroaki.osawa <hiroaki.osawa@smarthr.co.jp>
2022-09-22 19:09:54 +0900

    use ActiveRecord::Base.connection.schema_migration.table_name instead ActiveRecord::SchemaMigration.table_name for Rails 7.1 more

--- lib/database_cleaner/active_record/base.rb
+++ lib/database_cleaner/active_record/base.rb
@@ -15,7 +15,11 @@ module DatabaseCleaner
 
     class Base < DatabaseCleaner::Strategy
       def self.migration_table_name
-        ::ActiveRecord::SchemaMigration.table_name
+        if Gem::Version.new("6.0.0") <= ::ActiveRecord.version
+          ::ActiveRecord::Base.connection.schema_migration.table_name
+        else
+          ::ActiveRecord::SchemaMigration.table_name
+        end
       end
 
       def self.exclusion_condition(column_name)
